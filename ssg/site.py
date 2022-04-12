e1e031f90d769b1226619c8a51e5ffd701fb3924
David Cunliffe <david.cunliffe2@nhs.net>
2022-04-12 10:26:44 +0100

    fixing up versions so test work

--- ssg/site.py
+++ ssg/site.py
@@ -7,14 +7,11 @@ class Site:
         
     
     def create_dir(self, path):
-            directory = self.dest / Path(path).relative_to(self.source)
+            directory = self.dest / path.relative_to(self.source)
             directory.mkdir(parents=True, exist_ok=True)
     
     def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
-            if(path.is_dir() == True):
+            if path.is_dir():
                 self.create_dir(path)
-
-
-    
-
